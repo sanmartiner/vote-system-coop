1e4b682b44aa504ed5932f9b7d9ccffed18436fa
David San Martín <66731842+sanmartiner@users.noreply.github.com>
2023-12-17 10:58:16 -0300

    Controller adjustment for VotingSession endpoint.

--- src/main/java/com/coop/votingsystem/controller/VotingSessionControler.java
+++ src/main/java/com/coop/votingsystem/controller/VotingSessionControler.java
@@ -1,5 +1,6 @@
 package com.coop.votingsystem.controller;
 
+import com.coop.votingsystem.exceptionhandler.VotingSessionException;
 import com.coop.votingsystem.model.entitiy.VotingSession;
 import com.coop.votingsystem.model.entitiy.Topics;
 import com.coop.votingsystem.model.interfaces.TopicsService;
@@ -30,7 +31,7 @@ public class VotingSessionControler {
 
             LocalDateTime endSession = (end != null) ? end : LocalDateTime.now().plusMinutes(1);
 
-            VotingSession votingSession = votingSessionService.(topicId, endSession);
+            VotingSession votingSession = votingSessionService.openSession(topic, end);
 
             URI location = ServletUriComponentsBuilder
                     .fromCurrentRequest()
@@ -39,7 +40,7 @@ public class VotingSessionControler {
                     .toUri();
 
             return ResponseEntity.created(location).build();
-        } catch (SessaoVotacaoException e) {
+        } catch (VotingSessionException e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
