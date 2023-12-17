5502a1c9eff9d435162659ff55a92a14e6da0f02
David San Martín <66731842+sanmartiner@users.noreply.github.com>
2023-12-17 10:41:34 -0300

    Refactoring packages and adjustment for repositories Vote, Topic and Result

--- src/main/java/com/coop/votingsystem/repository/TopicRepository.java
+++ src/main/java/com/coop/votingsystem/repository/TopicRepository.java
@@ -1,9 +1,9 @@
 package com.coop.votingsystem.repository;
 
-import com.coop.votingsystem.model.Entitiy.Topics;
+import com.coop.votingsystem.model.entitiy.Topics;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TopicsRepository extends JpaRepository<Topics, Long> {
+public interface TopicRepository extends JpaRepository<Topics, Long> {
 }
