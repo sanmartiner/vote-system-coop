d39986c6a6920cc81474a677992e654728683997
David San Martín <66731842+sanmartiner@users.noreply.github.com>
2023-12-19 15:28:05 -0300

    Settings adjustment settings for rabbit mq

--- src/main/java/com/coop/votingsystem/integration/mensageria/RabbitMQConfig.java
+++ src/main/java/com/coop/votingsystem/integration/mensageria/RabbitMQConfig.java
@@ -1,11 +1,15 @@
-package com.coop.votingsystem.integration.mensageria.config;
+package com.coop.votingsystem.integration.mensageria;
 
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.FanoutExchange;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.backoff.ExponentialBackOffPolicy;
+import org.springframework.retry.support.RetryTemplate;
 
 @Configuration
 public class RabbitMQConfig {
@@ -24,4 +28,34 @@ public class RabbitMQConfig {
     public Binding binding(Queue votingResultQueue, FanoutExchange votingResultExchange) {
         return BindingBuilder.bind(votingResultQueue).to(votingResultExchange);
     }
+
+    @Bean
+    public CachingConnectionFactory rabbitConnectionFactory() {
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory("localhost");
+        connectionFactory.setUsername("developer");
+        connectionFactory.setPassword("dev123");
+
+        return connectionFactory;
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(CachingConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setRetryTemplate(retryTemplate());
+
+        return rabbitTemplate;
+    }
+
+    @Bean
+    public RetryTemplate retryTemplate() {
+        RetryTemplate retryTemplate = new RetryTemplate();
+        ExponentialBackOffPolicy backOffPolicy = new ExponentialBackOffPolicy();
+        backOffPolicy.setInitialInterval(500);
+        backOffPolicy.setMultiplier(2.0);
+        backOffPolicy.setMaxInterval(10000);
+        retryTemplate.setBackOffPolicy(backOffPolicy);
+
+        return retryTemplate;
+    }
 }
+
